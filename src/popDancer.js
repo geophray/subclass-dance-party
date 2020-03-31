e1cdefdee56379e0da773a21d3d9d62865a91e04
joshskkim <josh.sk.kim@gmail.com>
2020-03-30 17:32:37 -0700

    Initial popDancer commit

---
+++ src/popDancer.js
@@ -0,0 +1,13 @@
+var PopDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+}
+
+PopDancer.prototype = Object.create(Dancer.prototype);
+PopDancer.prototype.constructor = PopDancer;
+
+PopDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  this.$node.animate({
+    transform: rotateY(180deg);
+  });
+};

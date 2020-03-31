696c590911b35546d3860bc7466c9a49567acd17
Jeff Maughan <geophray@gmail.com>
2020-03-31 13:44:34 -0700

    Change squareDancer animation

--- src/squareDancer.js
+++ src/squareDancer.js
@@ -1,6 +1,7 @@
 var SquareDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node.addClass('animated square');
+  this.$node.addClass('square');
+  this.$node.css({ animation: 'rock ' + timeBetweenSteps + 'ms infinite' });
 };
 
 SquareDancer.prototype = Object.create(Dancer.prototype);
